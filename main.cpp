636a5a826aa5c6b00a5eb77298b61ace027b73c0
clarisson28 <reddieson05@gmail.com>
2020-07-17 20:00:36 +0800

    Drawing Shapes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
     return 0;
 }
