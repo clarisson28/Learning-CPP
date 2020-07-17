19523748f12aafe76dadf5231ec617d8bad19233
clarisson28 <reddieson05@gmail.com>
2020-07-17 20:17:33 +0800

    Variables

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "   /|" << endl;
-    cout << "  / |" << endl;
-    cout << " /  |" << endl;
-    cout << "/___|" << endl;
+    string characterName = "John";
+    int characterAge;
+    characterAge = 35;
+
+    cout << "There once was a man named " << characterName << "," << endl;
+    cout << "he was " << characterAge << " years old." << endl;
+    cout << "He really liked his name " << characterName << "," << endl;
+    cout << "but didn't like being " << characterAge << "." << endl;
     return 0;
 }
