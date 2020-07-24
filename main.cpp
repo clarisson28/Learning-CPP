471df01caa760d75e0771cb022d14c6dc861a685
clarisson28 <reddieson05@gmail.com>
2020-07-24 09:47:59 +0800

    Working with Numbers

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
-//#include <iostream>
-#include<stdio.h>
-#include<windows.h>
-#include<strings.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
 int main()
 {
-    system("cls");
-    printf("\n\n\t\t\t\tPlease Wait......Loading");
-    printf("\n\n");
-    for(int i=0;i<100;i++)
-    {
-        Sleep(12);
-        printf("%c",219);
-    }
-    printf("\n\n \t\t\t\t\t\t\tSuccessfully Loaded");
+    /*cout << 5 + 7;
+    cout << 5 - 7;
+    cout << 5 * 7;
+    cout << 5 / 7;*/
+    //cout << 10 % 3;
+    //cout << 4 + 5 * 10;
+    //cout << (4 + 5) * 10;
+
+    /*int wnum = 5;
+    double dnum = 5.5;
+
+    //wnum ++;
+    //wnum --;
+    //wnum += 80;
+    //wnum -= 80;
+    //wnum *= 80;
+    //wnum /= 80;
+    //cout << wnum;
+
+    //cout << 5.5 + 9;
+    //cout << 10 / 3;
+    //cout << 10.0 / 3.0;*/
+
+    //cout << pow(2, 5);
+    //cout << pow(3, 3);
+    //cout << sqrt(36);
+    //cout << round(4.3);
+    //cout << round(4.6);
+    //cout << ceil(4.1);
+    //cout << floor(4.8);
+    cout << fmax(3, 10);
 
     return 0;
 }
