c7d23f29bc305e0d68cea6a17bfcf9aeba18cbfa
clarisson28 <reddieson05@gmail.com>
2020-07-24 14:25:17 +0800

    More If Statements

--- main.cpp
+++ main.cpp
@@ -3,36 +3,51 @@
 
 using namespace std;
 
+/*
+int getMax(int num1, int num2){
+    int result;
+
+    if(num1 > num2){
+        result = num1;
+    }
+    else{
+        result = num2;
+    }
+
+    return result;
+}
 
 int main()
 {
-    //bool isMale = true;
-    //bool isTall = true;
-
-    //bool isMale = true;
-    //bool isTall = false;
-
-    //bool isMale = false;
-    //bool isTall = true;
-
-    bool isMale = false;
-    bool isTall = false;
-
-
-    if(isMale && isTall){
-        cout << "You are a tall male";
-    }
-    else if(isMale && !isTall){
-        cout << "You are a short male";
-    }
-    else if(!isMale && isTall){
-        cout << "You are tall but not male";
-    }
-    else
-    {
-        cout << "You are not male and not tall";
-    }
+    cout << getMax(2, 5);
 
     return 0;
 
+}
+*/
+
+int getMax(int num1, int num2, int num3){
+    int result;
+
+    if(num1 >= num2 && num1 >= num3){
+        result = num1;
+    }
+    else if(num2 >= num1 && num2 >= num3){
+        result = num2;
+    }
+    else{
+        result = num3;
+    }
+
+    return result;
+}
+
+int main()
+{
+    //cout << getMax(2, 5, 10);
+    //cout << getMax(2, 50, 10);
+    //cout << getMax(200, 50, 10);
+    cout << getMax(200, 200, 10);
+
+    return 0;
 }
