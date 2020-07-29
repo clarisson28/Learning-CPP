007ccafc6b23e91a987700e2ec59364c36811d2b
clarisson28 <reddieson05@gmail.com>
2020-07-29 14:19:49 +0800

    Building a Guessing Game

--- main.cpp
+++ main.cpp
@@ -5,10 +5,41 @@ using namespace std;
 
 int main()
 {
-    int index = 1;
-    while(index <= 5){
-        cout << index << endl;
-        index++;
+    /*
+    int secretNum = 7;
+    int guess;
+
+    while(secretNum != guess){
+        cout << "Enter guess: ";
+        cin >> guess;
+    }
+
+    cout << "You Win!";
+    */
+
+    int secretNum = 7;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
+
+    while(secretNum != guess && !outOfGuesses){
+        if(guessCount < guessLimit){
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        }
+        else {
+            outOfGuesses = true;
+        }
+
+    }
+
+    if(outOfGuesses){
+        cout << "You Lose!";
+    }
+    else {
+        cout << "You Win!";
     }
 
     return 0;
